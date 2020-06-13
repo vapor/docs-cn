77171fc467e5550d1834b50dcf43571ad1d172cb
Tanner Nelson <me@tanner.xyz>
2020-06-13 17:37:59 +0000

    update deploy workflow

--- build.sh
+++ build.sh
@@ -2,10 +2,10 @@ cd 4.0;
 mkdocs build;
 cd ..;
 
-rm -rf build
-mkdir -p build;
+rm -rf site
+mkdir -p site;
 
-mv 4.0/site build/4.0;
+mv 4.0/site site/4.0;
 
-echo "<meta http-equiv=\"refresh\" content=\"0; url=/4.0/\">" > build/index.html;
+echo "<meta http-equiv=\"refresh\" content=\"0; url=/4.0/\">" > site/index.html;
 
